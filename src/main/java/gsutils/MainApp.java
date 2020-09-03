051d50d2b555d1721a62adf797028f92c57babf0
Michael <mspellecacy@frakle.com>
2020-09-03 19:53:49 -0400

    Adding SysTray support. Right now to fully exit you must use the SysTray popup.

--- src/main/java/gsutils/MainApp.java
+++ src/main/java/gsutils/MainApp.java
@@ -11,8 +11,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.net.URL;
 
 
 public class MainApp extends Application {
@@ -22,7 +28,6 @@ public class MainApp extends Application {
     private static final Image APP_ICON = new Image(MainApp.class.getResourceAsStream("/images/gsutils.png"));
     private static final Logger log = LoggerFactory.getLogger(MainApp.class);
 
-
     private final GameSenseService gsService = GameSenseService.INSTANCE;
     private final PreferencesService prefsService = PreferencesService.INSTANCE;
 
@@ -30,7 +35,7 @@ public class MainApp extends Application {
         launch(args);
     }
 
-    public void start(Stage stage) throws Exception {
+    public void start(final Stage stage) throws Exception {
         log.info("Starting GSUtils");
 
         //First we must setup our endpoint.
@@ -63,12 +68,54 @@ public class MainApp extends Application {
         // Setups a way for us to open the user's browser for showing them docs.
         HostServicesService.INSTANCE.init(getHostServices());
 
+        //
+        Platform.setImplicitExit(false);
+
+        // sets up the tray icon (using awt code run on the swing thread).
+        setupSysTray(stage);
+    }
+
+    private void setupSysTray(final Stage stage) {
+        //Check the SystemTray is supported
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray is not supported");
+            return;
+        }
+
+        final URL trayIconURL = System.class.getResource("/images/gsutils.png");
+        final java.awt.Image trayIconImage = Toolkit.getDefaultToolkit().getImage(trayIconURL);
+        final SystemTray tray = SystemTray.getSystemTray();
+        TrayIcon trayIcon;
 
         //When the main app stage closes, shut down all the things. ALL THE THINGS!
         stage.setOnCloseRequest(e -> {
-            Platform.exit();
-            System.exit(0);
+            // Platform.exit();
+            // System.exit(0);
+            stage.hide();
+
         });
+
+        final ActionListener showListener = e -> Platform.runLater(stage::show);
+        final ActionListener closeListener = e -> System.exit(0);
+
+        // create a popup menu
+        PopupMenu popup = new PopupMenu();
+
+        MenuItem showItem = new MenuItem("Show");
+        showItem.addActionListener(showListener);
+        popup.add(showItem);
+
+        MenuItem closeItem = new MenuItem("Close");
+        closeItem.addActionListener(closeListener);
+        popup.add(closeItem);
+
+        trayIcon = new TrayIcon(trayIconImage, APP_NAME, popup);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException ex) {
+            System.out.println("TrayIcon could not be added.");
+        }
 
     }
 }
